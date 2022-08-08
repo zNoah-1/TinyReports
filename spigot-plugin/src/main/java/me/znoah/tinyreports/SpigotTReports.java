5b4a81bf50db410e047525038bdfbe93f18ec133
zNoah-1m <1maxter1pro@gmail.com>
2022-08-07 23:31:30 -0500

    Initial commit

---
+++ spigot-plugin/src/main/java/me/znoah/tinyreports/SpigotTReports.java
@@ -0,0 +1,15 @@
+package me.znoah.tinyreports;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotTReports extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
